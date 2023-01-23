b84bcde9b7d198daa12c4d5fc6936e6c225bcbff
Sharat Sachin <sharat.ss.sachin@gmail.com>
2023-01-23 01:44:07 +0000

    Add solution for climbing-stairs

---
+++ climbing-stairs/Solution.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/climbing-stairs
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        for(auto& x : dp) cout << x << " ";
+        cout << "\n";
+        return dp[n];
+    }
+};
