53ea17f96edf43e7c023d2fad48d14e3a66bc25f
Sharat Sachin <sharat.ss.sachin@gmail.com>
2023-02-13 07:36:49 +0000

    Add solution for count-odd-numbers-in-an-interval-range

---
+++ count-odd-numbers-in-an-interval-range/Solution.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        return ((high - low + 1) / 2) + (low %2 && high % 2);
+    }
+};
