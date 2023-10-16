38839d878db79322a203dea5cac776cbecfdb0af
Sharat Sachin <sharat.ss.sachin@gmail.com>
2023-10-16 10:31:45 +0000

    Add solution for search-insert-position

---
+++ search-insert-position/Solution.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/search-insert-position
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int x = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return x;
+    }
+};
