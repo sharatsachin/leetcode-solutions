ba06814e890db17359104268aa543734a38afe52
Sharat Sachin <sharat.ss.sachin@gmail.com>
2023-11-06 07:50:03 +0000

    Add solution for seat-reservation-manager

---
+++ seat-reservation-manager/Solution.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/seat-reservation-manager
+
+class SeatManager {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    SeatManager(int n) {
+        pq = priority_queue<int, vector<int>, greater<int>>();
+        for(int i = 1; i <= n; i++) {
+            pq.push(i);
+        }
+    }
+    
+    int reserve() {
+        int x = pq.top();
+        pq.pop();
+        return x;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
